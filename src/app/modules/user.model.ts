2d515c9fc0044a76963375ecf27ba52d4649dae1
Shohidul Arifin <shohidularifin@gmail.com>
2023-11-24 07:09:49 -0800

    interface, schema and model created

---
+++ src/app/modules/user.model.ts
@@ -0,0 +1,30 @@
+import { IUser } from './user/user.interface';
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema<IUser>({
+  userId: { type: Number },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  fullName: {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+  },
+  age: { type: Number, required: true },
+  email: { type: String, required: true },
+  isActive: { type: Boolean },
+  hobbies: [{ type: String }, { type: String }],
+  address: {
+    street: { type: String },
+    city: { type: String },
+    country: { type: String },
+  },
+  orders: [
+    {
+      productName: { type: String },
+      price: { type: Number },
+      quantity: { type: Number },
+    },
+  ],
+});
+
+const User = model<IUser>('User', userSchema);
---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,25 @@
+export type IUser = {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: {
+    firstName: string;
+    lastName: string;
+  };
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: [string, string];
+  address: {
+    street: string;
+    city: string;
+    country: string;
+  };
+  orders: [
+    {
+      productName: string;
+      price: number;
+      quantity: number;
+    },
+  ];
+};
